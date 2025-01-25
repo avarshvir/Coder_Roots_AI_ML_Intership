7dc8da3d68d8c10bf8b4c123cb9bbfa7ca5e26a0
Arshvir <avarshvir@gmail.com>
2025-01-25 12:20:11 +0530

    ques_2.py
    if else, for loop

---
+++ 2_if_else_for_loop_task/ques_2.py
@@ -0,0 +1,23 @@
+#Question 2
+#WAP Ask user to input 2 number,
+#tell the followings
+#1. Both number are equal or not
+#2. Which is Bigger in both
+#3. Either First NUmber is smaller or equal to Second Number
+#4. Print your first name 5 times is first number is
+#   greather than second and print your surname 3 times if 1st no
+#   is less than second
+
+num_1 = int(input("Enter 1st number: "))
+num_2 = int(input("Enter 2nd number: "))
+
+if(num_1 == num_2):
+    print("Both numbers are equal")
+elif(num_1 > num_2):
+    print("Number 1 is greater")
+    for i in range(5):
+        print("Arshvir")
+else:
+    print("Number 2 is greater")
+    for i in range(3):
+        print("Naranch")
