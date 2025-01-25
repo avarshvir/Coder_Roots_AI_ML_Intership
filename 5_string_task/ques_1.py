7a188d8f3daca4d519e9413bdbabf81ecfa212d8
Arshvir <avarshvir@gmail.com>
2025-01-25 14:46:09 +0530

    string task ques1.py

--- 5_string_task/ques_1.py
+++ 5_string_task/ques_1.py
@@ -6,12 +6,11 @@ Sample string - “New year”
 Expected result - “near” """
 
 str_1 = input("Enter 1st String: ")
-str_2 = input("Enter 2nd String: ")
 
-if len(str_1) < 2 and len(str_2) < 2:
+if len(str_1) < 2:
     print("not a valid string")
 else:
     a = str_1[:2]
-    b = str_2[-2:]
-    if str_1.capitalize() or str_2.capitalize():
+    b = str_1[-2:]
+    if str_1.capitalize():
         print(a.lower()+b.lower())
