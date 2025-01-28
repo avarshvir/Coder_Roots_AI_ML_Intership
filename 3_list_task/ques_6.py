b9058ee3eae0bb63f70c46dbea704b78c7e82dc5
Arshvir <avarshvir@gmail.com>
2025-01-28 15:37:08 +0530

    list task question 6
    location, index, list, update

--- 3_list_task/ques_6.py
+++ 3_list_task/ques_6.py
@@ -11,13 +11,16 @@ Ex: [1,2,3,4,5]
 --> Please location for billa: 1
 [1,"Billa",3,4,5,"Raju"]"""
 
-my_list = [1,2,3,4,5]
+my_list = ['abc','xyz','qwe','rty','jkl']
 print(my_list)
 friend_input = input("Enter friend name: ")
 my_list.append(friend_input)
 print(my_list)
 
 another_friend = input("Enter most important friend: ")
-location_input = int(input(f"Enter location for {another_friend: }"))
+location_input = int(input("Enter location: "))
 
+my_list[location_input] = another_friend
+
+print(my_list)
 
