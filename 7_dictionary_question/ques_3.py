b93ccc9d18e352ec5f3f391fdce102f54df079f0
Arshvir <avarshvir@gmail.com>
2025-01-29 13:24:12 +0530

    ques_3.py

---
+++ 7_dictionary_question/ques_3.py
@@ -0,0 +1,17 @@
+# Write a program for a number guessing game where the computer randomly selects a number between 1 and 100, 
+# and the user tries to guess it. The program should give hints if the guess is too high or too low.
+
+import random
+
+user_input = int(input("Enter the Number: "))
+
+computer_guess = random.randint(1,100)
+
+print("Computer guess the number is: ",computer_guess)
+
+if user_input == computer_guess:
+    print("numbers are same")
+elif computer_guess >= user_input:
+    print("number is too high that is guess by computer") 
+else:
+    print("number is too low that is guess by cmputer") 
