73e5951df1953b59e23fb6fbc7ed1d6547d4f8ef
Arshvir <avarshvir@gmail.com>
2025-02-04 11:19:21 +0530

    commit

--- test_1/ques_4.py
+++ test_1/ques_4.py
@@ -3,5 +3,7 @@ if it is divisible by 4, but not by 100, unless it is also divisible by 400."""
 
 year_in = int(input("Enter year: "))
 
-if year_in % 4 == 0:
-    print(year_in," Year is Leap Year")
+if (year_in % 4 == 0 and year_in % 100 != 0) or (year_in % 400 == 0):
+    print(year_in, "is a Leap Year")
+else:
+    print(year_in, "is NOT a Leap Year")
