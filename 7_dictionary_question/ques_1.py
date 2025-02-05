58c6955a72e1245a12664ac73d192eac24763389
Arshvir <avarshvir@gmail.com>
2025-02-06 00:28:22 +0530

    ques_1.py

--- 7_dictionary_question/ques_1.py
+++ 7_dictionary_question/ques_1.py
@@ -1,2 +1,14 @@
 #/Q1 Take a empty Dictionary and Ask user to input the number of time they want to enter and 
 # input form user to make dict
+
+
+my_dict = {}
+
+num = int(input("How many key-value pairs do you want to add? "))
+
+for i in range(num):
+    key = input(f"Enter key {i + 1}: ")
+    value = input(f"Enter value for {key}: ")
+    my_dict[key] = value 
+
+print("Final Dictionary:", my_dict)
