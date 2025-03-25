a56eee7d3e3629741c3992e4484f4b02a46f9d45
avarshvir <avarshvir@gmail.com>
2025-03-25 23:50:52 +0530

    commit

---
+++ projects/house_price_prediction/house_price_pred_ui.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import numpy as np
+
+st.set_page_config(page_title="House Price Prediction", layout="wide")
+
+st.header("House Price Prediction using Linear Regression")
+st.divider()
+
+col1, col2,col3, col4 = st.columns((1,1,1,1))
+
+with col1:
+    area = st.number_input("Area",placeholder="Enter Area")
+
+with col2:
+    bedrooms = st.number_input("BedRoom",placeholder="Enter no. of bedrooms")
+
+with col3:
+    bathrooms = st.number_input("Bathroom",placeholder="Enter no. of bathrooms")
+
+with col4:
+    stories = st.number_input("Stories",placeholder="Enter no. of stories")
+
+arr = np.array([area,bathrooms,bathrooms,stories])
+
+import joblib
+
+predictive_price = joblib.load('housing2.pkl')
+
+
+def p():
+    pp = predictive_price.predict([arr])
+    st.write(pp)
+
+st.button("Predict",on_click=p)
